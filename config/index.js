c70fe9228ee7d35a26bc88746cbd6d66aae73c71
19534212@qq.com <hxfq5166>
2018-01-21 01:41:57 +0800

    update docs/index

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'https://omiomi.github.io/nic/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
